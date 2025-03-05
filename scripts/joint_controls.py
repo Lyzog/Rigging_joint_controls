8b07e0c6b621abc615d0abd10ea05ed8bda9f629
Lyzog <xddaniel999@gmail.com>
2025-03-05 20:21:23 +0100

    created the script for creating FK Controls for selected joints in a scene in maya and structure the hierarchy when creating the controls and groups

--- scripts/joint_controls.py
+++ scripts/joint_controls.py
@@ -1,2 +1,22 @@
 import maya.cmds as cmds
 
+def createControls(name="FK_Controller", radius=20, controllerNormal=[1, 0, 0]):
+    selection = cmds.ls(selection=True)
+    if not selection:
+        cmds.warning("No joints selected!")
+        return
+
+    previousController = None
+
+    for joint in selection:
+        controller = cmds.circle(nr=controllerNormal, r=radius, n=joint + "_" + name)[0]
+        controllerGroup = cmds.group(controller, n=joint + "_Group")
+        cmds.delete(cmds.parentConstraint(joint, controllerGroup, mo=False))
+        cmds.orientConstraint(controller, joint, mo=False)
+
+        if previousController:
+            cmds.parent(controllerGroup, previousController)
+
+        previousController = controller
+
+createControls()
